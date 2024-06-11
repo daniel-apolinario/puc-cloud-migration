72f629ddf4c982f82e6ff8afb23f4e7eac3a7ceb
Daniel Apolinario <daniel.apolinario@embrapa.br>
2024-06-11 14:01:22 -0300

    Aplicação Hello World

---
+++ src/test/java/puc/cloud/migration/puc_cloud_migration/PucCloudMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package puc.cloud.migration.puc_cloud_migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PucCloudMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
